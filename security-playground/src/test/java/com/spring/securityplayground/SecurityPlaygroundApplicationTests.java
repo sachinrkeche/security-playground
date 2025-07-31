037aa3672322c071f76bfa5037b8096d5467eec2
Sachin Keche <sachinrkeche@gmail.com>
2025-07-31 15:15:16 +0530

    skeleton of project

---
+++ security-playground/src/test/java/com/spring/securityplayground/SecurityPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.securityplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
