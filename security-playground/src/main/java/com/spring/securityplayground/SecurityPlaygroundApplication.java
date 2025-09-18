4dc52a3156a0fb7fb1e8e634b50ce11311310643
Sachin Keche <sachinrkeche@gmail.com>
2025-09-18 16:12:47 +0530

    request validation filter added.

--- security-playground/src/main/java/com/spring/securityplayground/SecurityPlaygroundApplication.java
+++ security-playground/src/main/java/com/spring/securityplayground/SecurityPlaygroundApplication.java
@@ -5,10 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 import java.util.Arrays;
 
 @SpringBootApplication
+@EnableWebSecurity(debug = true)
 public class SecurityPlaygroundApplication {
 
 	@Autowired
